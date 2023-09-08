7a73998f576caf2082d996f940560370ad34a026
hyeri-woo <woohyeri0525@gmail.com>
2023-09-08 22:18:48 +0900

    Feat: context provider 설정

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import App from '../App';
 import Home from '../pages/Home';
 import { createBrowserRouter } from 'react-router-dom';
 import NotFound from '../pages/NotFound';
+import SearchProvider from '../context/SearchProvider';
 
 export const router = createBrowserRouter([
   {
@@ -10,7 +11,11 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
+        element: (
+          <SearchProvider>
+            <Home />
+          </SearchProvider>
+        ),
       },
     ],
     errorElement: <NotFound />,
